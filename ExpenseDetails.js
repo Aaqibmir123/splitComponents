35358881a17c82f62c8679d3082f4d7015cbb44f
Mir Aaqib Bashir <116267216+Aaqibmir123@users.noreply.github.com>
2022-12-16 22:17:42 +0530

    Add files via upload

---
+++ ExpenseDetails.js
@@ -0,0 +1,12 @@
+function ExpenseDetails(){
+    const title="Insurance";
+    const amount=55.00;
+    return(
+        <div>
+            <div>{title}</div>
+            <div>{amount}</div>
+        </div>
+
+    );
+}
+export default ExpenseDetails;
